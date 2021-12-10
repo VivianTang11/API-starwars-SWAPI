ebb39447a8c8d897fafe47aacfb5dd1e23b052a1
VivianTang11 <vivian.tang@hotmail.se>
2021-12-10 17:22:53 +0100

    finished comments in app.js

--- scripts/app.js
+++ scripts/app.js
@@ -9,13 +9,13 @@ const loaderModal = document.getElementById('movie-modal-loader')
 
 let films = []
 
-//async/await function that waits untill url is fetched and result from swapi.com.
-//Then the result is moved/stored into empty array called films, then sorted using conditional tenary operator in release date (latest to earliest). 
-//rendent film titles & release dates on every element/object from array film. Also a onclick event is rendent on each film with their index as the argument.
+//async/await function that waits untill url is fetched/request and respond from https://swapi.dev/api/films.
+//Then the result is moved/stored into empty array (variable films) & change styling for loader to display none. 
+//Then it rendents each film titles & release dates on every element/object, also a onclick event is rendent on each film (with its index from array as the argument/parameter) .
 const getFilms = async () => {
     const res = await fetch('https://swapi.dev/api/films')
     const data = await res.json()
-    films = data.results.sort((a, b) => (a.release_date > b.release_date) ? 1 : -1)
+    films = data.results
     loader.style.display = 'none'
     movies.innerHTML = 
     films.map((film, index)=> 
@@ -24,7 +24,7 @@ const getFilms = async () => {
         </div>`).join(' ') 
 }
 
-//function that opens a modal when a film is clicked and index 'i' follows into function modalContents(i). 
+//function that opens a modal when a film is clicked, changes styling for movieModal to flex, and index 'i' that was clicked follows into function modalContents(i). 
 const openMovieModal = () => {
     for (let i = 0; i < movieRelease.length; i++) {
         movieRelease[i].addEventListener('click', () => {
@@ -35,7 +35,7 @@ const openMovieModal = () => {
 }
 
 //function that rendens each character from array film and index with async/await function. Then retuns promises.
-//This is to fetch each URL for every characters depending on which film index 'i'.
+//This is to fetch each URL for every characters depending on which film index was followed. This will be used in function modalContents.
 const charPromises = (index) => {
     const promises = films[index].characters.map( async (url) => {
         const res = await fetch(url)
@@ -45,7 +45,8 @@ const charPromises = (index) => {
 }
 
 //async function catched all promises from function charPromises(), followed with the argument index.
-//then sorts the characters in alfabetical order before innerHTML is filled with content (title, img, opening crawl & character name) for the index 
+//then sorts the characters in alfabetical order before innerHTML is filled with content (title, img, opening crawl & character name) for the specific index that was clicked/followed from before.
+//then styling, loaderModal changes to none, modalContainer to block & modalContent to flex after above is completed (description in row 47 & 48),
 const modalContents = async (index) => {
     const res = await Promise.all(charPromises(index))
     res.sort((a, b) => (a.name > b.name) ? 1 : -1)
@@ -57,14 +58,15 @@ const modalContents = async (index) => {
         </div>
         <div id="modal-right">
             <h1>Characters:</h1>`+ 
-            res.map((arr) => `<p>${arr.name}</p>`).join('') + 
+            res.map((character) => `<p>${character.name}</p>`).join('') + 
         `</div>`
     loaderModal.style.display = 'none'
     modalContainer.style.display = 'block'
     modalContent.style.display = 'flex'
 }
 
-//close modal when symbol "x" is clicked
+//close modal when symbol "x" is clicked in the modal.
+//then styling, moveModal & modalContainer to none. loaderModal to flex.
 const closeMovieModal = () => {
     closeBtn.addEventListener('click', () => {
         movieModal.style.display = 'none'
